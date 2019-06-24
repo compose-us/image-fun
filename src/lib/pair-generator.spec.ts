3aa66ac65abea1756627297b734d2a160f8830a6
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-06-24 23:55:47 +0200

    Add pairResolver function and type it nicely
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/pair-generator.spec.ts
+++ src/lib/pair-generator.spec.ts
@@ -1,5 +1,13 @@
+import pairResolver from "./pair-resolver";
+
 describe("pair-resolver", () => {
-  it("test setup works", () => {
-    expect(true).toBe(true);
+  it("does not raise an exception when called", () => {
+    expect(pairResolver).not.toThrow();
+  });
+
+  it("returns a pair of non-empty strings", () => {
+    const result = pairResolver();
+    expect(result[0]).not.toBe("");
+    expect(result[1]).not.toBe("");
   });
 });
---
+++ src/lib/pair-resolver.ts
@@ -0,0 +1,3 @@
+export default function pairResolver(): [String, String] {
+  return ["", ""];
+}
