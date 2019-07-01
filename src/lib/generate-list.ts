2db5b44c7d76f52c1b7af2f71fe5a1fca3de1178
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-07-01 22:23:56 +0200

    Make sure to equally distribute the words
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/generate-list.ts
+++ src/lib/generate-list.ts
@@ -1,8 +1,18 @@
 const generateList = (words: string[], num: number) => {
-  let keywords = [];
-  for (let i = 0; i < num; i++) {
-    keywords.push(words[Math.floor(Math.random() * words.length)]);
+  const maxAmountPerWord = Math.floor(num / words.length);
+  let freeSpace = new Array(num).fill(1).map((i, idx) => i * idx);
+  let keywords = new Array(num);
+  for (let word of words) {
+    for (let i = 0; i < maxAmountPerWord; i++) {
+      const selectedIndex =
+        freeSpace[Math.floor(Math.random() * freeSpace.length)];
+      keywords[selectedIndex] = word;
+      freeSpace = freeSpace.filter(index => index !== selectedIndex);
+    }
   }
+  freeSpace.forEach(remainingIndex => {
+    keywords[remainingIndex] = words[Math.floor(Math.random() * words.length)];
+  });
   return keywords;
 };
 
