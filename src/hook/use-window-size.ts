58c9f28df64979aacb337779e2c9771d3a7973a8
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-07-19 00:24:40 +0200

    Move resize check into separate function To keep the useEffect or useLayoutEffect function as small as possible, we can separate out the functionality of doing the check / setting the geometry
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/hook/use-window-size.ts
+++ src/hook/use-window-size.ts
@@ -3,11 +3,13 @@ import { useState, useLayoutEffect } from "react";
 const useWindowSize = () => {
   const [height, setHeight] = useState(0);
   const [width, setWidth] = useState(0);
+
+  const resizeCheck = () => {
+    setHeight(window.innerHeight);
+    setWidth(window.innerWidth);
+  };
+
   useLayoutEffect(() => {
-    const resizeCheck = () => {
-      setHeight(window.innerHeight);
-      setWidth(window.innerWidth);
-    };
     window.addEventListener("resize", resizeCheck);
     resizeCheck();
     return () => {
