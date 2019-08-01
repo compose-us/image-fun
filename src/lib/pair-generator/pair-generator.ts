e7f815dad7ebf17acbddbc8148865a15c4600f1f
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-08-01 07:01:41 +0200

    Move pairs into own module
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/pair-generator/pair-generator.ts
+++ src/lib/pair-generator/pair-generator.ts
@@ -1,25 +1,4 @@
-const listOfPairs: [string, string][] = [
-  ["birthday", "cake"],
-  ["cheese", "cake"],
-  ["coal", "mine"],
-  ["coffee", "shop"],
-  ["doggy", "bag"],
-  ["elementary", "school"],
-  ["grass", "roots"],
-  ["iron", "man"],
-  ["lion", "king"],
-  ["money", "bag"],
-  ["monkey", "wrench"],
-  ["mountain", "bike"],
-  ["photo", "shoot"],
-  ["shopping", "cart"],
-  ["space", "ship"],
-  ["taxi", "driver"],
-  ["traffic", "light"],
-  ["travel", "guide"],
-  ["tv", "channel"],
-  ["water", "cooler"]
-];
+import listOfPairs from "../../service/pairs";
 
 export default function pairGenerator(): [string, string] {
   return listOfPairs[Math.floor(Math.random() * listOfPairs.length)];
---
+++ src/service/pairs.ts
@@ -0,0 +1,24 @@
+const listOfPairs: [string, string][] = [
+  ["birthday", "cake"],
+  ["cheese", "cake"],
+  ["coal", "mine"],
+  ["coffee", "shop"],
+  ["doggy", "bag"],
+  ["elementary", "school"],
+  ["grass", "roots"],
+  ["iron", "man"],
+  ["lion", "king"],
+  ["money", "bag"],
+  ["monkey", "wrench"],
+  ["mountain", "bike"],
+  ["photo", "shoot"],
+  ["shopping", "cart"],
+  ["space", "ship"],
+  ["taxi", "driver"],
+  ["traffic", "light"],
+  ["travel", "guide"],
+  ["tv", "channel"],
+  ["water", "cooler"]
+];
+
+export default listOfPairs;
