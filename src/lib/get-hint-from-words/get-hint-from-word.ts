ee39ac5e454057a89a7cf3c3bc4f6655884fe397
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-08-22 17:20:10 +0200

    Use different approach for hint function
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/lib/get-hint-from-words/get-hint-from-word.ts
+++ src/lib/get-hint-from-words/get-hint-from-word.ts
@@ -5,13 +5,14 @@ const getHintFromWord: GetHintFromWord = (
   amountOfHintChars = Math.floor(word.length / 3)
 ) => {
   const chars = word.split("");
-  const hintChars = chars.slice(0, amountOfHintChars).join("");
-  const rest = Array.from({
-    length: chars.length - amountOfHintChars
-  })
-    .map(() => ".")
-    .join("");
-  return `${hintChars}${rest}`;
+  let indices = chars.map((_, idx) => idx);
+  let hint = chars.map(() => ".");
+  for (let i = 0; i < amountOfHintChars; i++) {
+    const selectedIndex = indices[Math.floor(Math.random() * indices.length)];
+    hint[selectedIndex] = chars[selectedIndex];
+    indices = indices.filter(index => index !== selectedIndex);
+  }
+  return hint.join("");
 };
 
 export default getHintFromWord;
