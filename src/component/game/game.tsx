96e8688944984b960dd4d42acf4f407e3f87343d
Philip Saa <cowglow@gmail.com>
2020-12-03 23:30:26 +0100

    WIP: Image display

--- src/component/game/game.tsx
+++ src/component/game/game.tsx
@@ -11,7 +11,8 @@ const Game: React.FC = () => {
   const { compoundWord } = useGameState();
 
   const [height, width] = useWindowSize();
-  const sizeX = Math.ceil(width / 2);
+  // TODO: if the width is less than 600, the layout should wrap
+  const sizeX = (width > 600) ? Math.ceil(width / 2): width;
   const sizeY = Math.ceil(height / 2);
   const images = useMemo(() => {
     return compoundWord.map((keyword) =>
@@ -32,12 +33,12 @@ const Game: React.FC = () => {
         </div>
         <ResetWindow />
       </div>
-      <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
         {images.map((image, index) => (
           <Image
             key={image.url}
-            x={index % 6}
-            y={Math.floor(index / 6)}
+            x={index % 2}
+            y={Math.floor(index / 2)}
             src={image.url}
             width={image.width}
             height={image.height}
