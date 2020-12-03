fff3d1a8013195b962f763008f8325f7049f815b
Philip Saa <cowglow@gmail.com>
2020-12-03 21:18:52 +0100

    Moves hint display to compound word display component

---
+++ src/component/compound-word-display/compound-word-display.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./compound-word-display.module.css";
+
+interface CompoundWordDisplayProps {
+  word: string;
+}
+
+const CompoundWordDisplay: React.FC<CompoundWordDisplayProps> = ({ word }) => {
+  const letters: string[] = word.split("");
+  return (
+    <div className={styles.root}>
+      {letters.map((letter) => (
+        <input
+          type="text"
+          value={letter}
+          className={styles.input}
+          disabled={letter === "*"}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CompoundWordDisplay;
