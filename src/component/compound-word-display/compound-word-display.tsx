d0b89e4869c45ba67eae9acebb7c40770f498583
Joern Bernhardt <joern.bernhardt@compose.us>
2020-12-10 14:31:35 +0100

    Remove warning for readOnly field
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/component/compound-word-display/compound-word-display.tsx
+++ src/component/compound-word-display/compound-word-display.tsx
@@ -9,13 +9,15 @@ const CompoundWordDisplay: React.FC<CompoundWordDisplayProps> = ({ word }) => {
   const letters: string[] = word.split("");
   return (
     <div className={styles.root}>
-      {letters.map((letter) => (
+      {letters.map((letter, index) => (
         <input
+          key={index}
           tabIndex={-1}
           type="text"
           value={letter}
           className={styles.input}
           disabled={letter === "*"}
+          readOnly
         />
       ))}
     </div>
