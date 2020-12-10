970fa582faa03c1325bdc4750d04c227c9945b07
Philip Saa <cowglow@gmail.com>
2020-12-10 19:18:34 +0100

    Remove comment

--- src/lib/get-hint-from-words/get-hint-from-words.ts
+++ src/lib/get-hint-from-words/get-hint-from-words.ts
@@ -3,7 +3,6 @@ import getHintFromWord from "./get-hint-from-word";
 type GetHintFromWords = (...words: string[]) => string[];
 
 const getHintFromWords: GetHintFromWords = (...words) => {
-  // const word = words[Math.floor(Math.random() * words.length)];
   const word1 = words[0];
   const word2 = words[1];
   return [getHintFromWord(word1), getHintFromWord(word2)];
